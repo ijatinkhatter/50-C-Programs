98d13fca07d3a90845899d043b514942402810d7
Jatin Khatter <142179590+ijatinkhatter@users.noreply.github.com>
2023-08-19 20:23:22 +0530

    Create main.c

---
+++ Program 5/main.c
@@ -0,0 +1,10 @@
+// Write a Program to find ASCII values of a Character
+#include <stdio.h>
+int main(){
+    char ch;
+    int n = ch;
+    printf("Enter any character : ");
+    scanf("%c", &ch);
+    printf("The ASCII value of character is %d", ch);
+    return 0;
+}
